ba17e6cf2c897a4a5feca3549dd049ba7f5c0485
Evert Ortiz <evert.ortiz.m@gmail.com>
2022-08-16 09:03:16 -0500

    refactor: Separate the database code

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,10 +1,13 @@
 const express = require('express');
+
+const response = require('../../../network/response');
+const Controller = require('./controller');
+
 const router = express.Router();
 
-const response = require('../../../network/response')
-
-router.get('/', function(req, res) {
-  response.success(req, res, 'todo correcto', 200)
+router.get('/', function (req, res) {
+    const lista = Controller.list();
+    response.success(req, res, lista, 200);
 })
 
 module.exports = router;
